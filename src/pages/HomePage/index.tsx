07d6abd00f1645c162985e3a1bd23ec557946174
Kunal Gosrani <kunalgosrani10@gmail.com>
2024-06-17 04:43:52 +0530

    feat: logout

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,9 @@
 import { Button, Container, Text, Title } from "@mantine/core";
 import { AccountListView } from "./AccountListView";
+import { useNavigate } from "react-location";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <Container className="mt-10">
       <div className="flex flex-col gap-4 max-w-lg mx-auto">
@@ -10,8 +12,15 @@ export const HomePage = () => {
           <Text className="text-gray-600">
             Manage all your linked email accounts
           </Text>
-          <div className="mt-2">
+          <div className="mt-2 flex flex-row justify-between">
             <Button>Add Email Account</Button>
+            <Button
+              variant="light"
+              color="red"
+              onClick={() => navigate({ to: "/logout" })}
+            >
+              Logout
+            </Button>
           </div>
         </div>
         <AccountListView />
